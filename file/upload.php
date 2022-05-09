c05683c2cb41875e1d66d733584ad20f7ca01e87
zyakal <pkd1426@gmail.com>
2022-05-09 11:36:04 +0900

    이미지 업로드

---
+++ file/upload.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="upload_proc.php" method="POST" enctype="multipart/form-data">
+        <div><label>이미지 <input type="file" name="img" accept="image/*"></label></div>
+        <div><input type="submit" value="업로드">
+    </form>
+</body>
+</html>
